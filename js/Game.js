c1535fcd6da7c8b73461e96ccc153fc0526716a9
RobertMJr <25443406+RobertMJr@users.noreply.github.com>
2020-07-21 08:53:53 +0300

    WIP

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,34 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+class Game {
+    constructor() {
+        this.missed = 0;
+        this.phrases = this.createPhrases();
+        this.activePhrase = null;
+    }
+    /**
+    * Creates phrases for use in game
+    * @return {array} An array of phrases that could be used in the game
+    */
+    createPhrases() {
+        const phrasesArr = [];
+        const listOfPhrases = ['Life is like a box of chocolates', 'Until we meet again', 'I am what I am', 'More power to you', 'Hitch Your Wagon to a Star'];
+        for(let i = 0; i < 5; i += 1){
+            let phraseObject = new Phrase(listOfPhrases[i]);
+            phrasesArr.push(phraseObject);
+        }
+        return phrasesArr;
+    };
+
+    
+    /**
+    * Selects random phrase from phrases property
+    * @return {Object} Phrase object chosen to be used
+    */
+    getRandomPhrase() {
+        return this.phrases[Math.floor(Math.random() * (this.phrases.length))];
+    };    
+
+}
--- js/Phrase.js
+++ js/Phrase.js
@@ -1,3 +1,32 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Phrase.js */
+
+ class Phrase {
+    constructor(phrase) {
+        this.phrase = phrase.toLowerCase();   
+    }
+
+    /**
+    * Display phrase on a game board
+    */
+    addPhraseToDisplay() {
+        const unorderedList = document.querySelector('ul');
+        [...this.phrase].forEach(letter => {
+            const listItem = document.createElement('li');
+            if(letter !== ' '){ 
+                listItem.classList.add(`hide`);
+                listItem.classList.add(`letter`);
+                listItem.classList.add(`${letter}`);
+                listItem.innerHTML = letter;
+                unorderedList.appendChild(listItem);
+            } else {
+                listItem.classList.add(`space`);
+                listItem.innerHTML = ' ';
+                unorderedList.appendChild(listItem);
+            }
+        })
+    }
+
+
+ }
